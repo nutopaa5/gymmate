4556c5b1d4fddad022853cf53bd2839622f65a5e
Neuvonen Joonas <a1702686@myy.haaga-helia.fi>
2019-03-27 09:29:56 +0200

    First commit

---
+++ src/test/java/hh/swd20/gymmate/GymmateApplicationTests.java
@@ -0,0 +1,16 @@
+package hh.swd20.gymmate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GymmateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
